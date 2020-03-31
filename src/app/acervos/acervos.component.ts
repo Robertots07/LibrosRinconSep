118e2670758e75bff372b4af80ddeb46560ec048
iuka <roberto.t37@gmail.com>
2020-03-31 13:46:28 -0600

    Create acervos.component.ts

---
+++ src/app/acervos/acervos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acervos',
+  templateUrl: './acervos.component.html',
+  styleUrls: ['./acervos.component.css']
+})
+export class AcervosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
