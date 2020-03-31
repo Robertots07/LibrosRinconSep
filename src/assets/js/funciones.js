2a832b97d33bc0d3c13f72cc766e509ee5580913
iuka <roberto.t37@gmail.com>
2020-03-31 13:47:24 -0600

    Create funciones.js

---
+++ src/assets/js/funciones.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    /*aqui rebe mete lo javascript*/
+    new WOW().init();
+
+    $('.ir-arriba').click(function() {
+        $('body, html').animate({
+            scrollTop: '0px'
+        }, 300);
+    });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 0) {
+            $('.ir-arriba').slideDown(300);
+        } else {
+            $('.ir-arriba').slideUp(300);
+        }
+    });
+
+
+
+    /* Logo Slider */
+    $('.carousel.carousel-multi-item.v-2 .carousel-item').each(function() {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        for (var i = 0; i < 4; i++) {
+            next = next.next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+            next.children(':first-child').clone().appendTo($(this));
+        }
+    });
+    //side bar 
+    $(document).on("click", "#sidebarCollapse", function() {
+        $('#sidebar').toggleClass('active');
+        $(this).toggleClass('active');
+    });
+    /*aqui rebe termina lo javascript*/
+});
