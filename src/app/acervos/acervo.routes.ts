6c18d0c55d63b1931321295561c824f33887f9b0
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-02 16:30:06 -0600

    Update acervo.routes.ts

--- src/app/acervos/acervo.routes.ts
+++ src/app/acervos/acervo.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeAcervosComponent } from './home-acervos/home-acervos.component';
 import { SobrecoleccionComponent } from './sobrecoleccion/sobrecoleccion.component';
+import { SeriesComponent } from './series/series.component';
+
 
 export const ACERVOS_ROUTES: Routes = [
 
     { path: 'HomeAc', component: HomeAcervosComponent },
     { path: 'SobreColeccion', component: SobrecoleccionComponent },
+    { path: 'Series', component: SeriesComponent },
     { path: '**', component: HomeAcervosComponent }
   ];
