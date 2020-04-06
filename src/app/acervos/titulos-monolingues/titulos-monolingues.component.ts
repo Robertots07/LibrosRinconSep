85a7b7ea6b54565171c2d9c61f788fbc3244fd18
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-06 16:43:34 -0500

    Create titulos-monolingues.component.ts

---
+++ src/app/acervos/titulos-monolingues/titulos-monolingues.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-titulos-monolingues',
+  templateUrl: './titulos-monolingues.component.html',
+  styleUrls: ['./titulos-monolingues.component.css']
+})
+export class TitulosMonolinguesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
