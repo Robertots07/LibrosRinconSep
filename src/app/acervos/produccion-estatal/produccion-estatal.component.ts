5818168aec19c2955f78a5491d9b90fa49955e99
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-06 16:43:12 -0500

    Create produccion-estatal.component.ts

---
+++ src/app/acervos/produccion-estatal/produccion-estatal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produccion-estatal',
+  templateUrl: './produccion-estatal.component.html',
+  styleUrls: ['./produccion-estatal.component.css']
+})
+export class ProduccionEstatalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
