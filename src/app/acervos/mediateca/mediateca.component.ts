edc53666ac5bc56497e4c2e7ec295f915bdc3b9d
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-07 17:43:57 -0500

    Create mediateca.component.ts

---
+++ src/app/acervos/mediateca/mediateca.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mediateca',
+  templateUrl: './mediateca.component.html',
+  styleUrls: ['./mediateca.component.css']
+})
+export class MediatecaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
