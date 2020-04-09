234d63fc82335ab7510de0c74e47163cbf5309de
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-08 19:12:46 -0500

    Create home-alumnos.component.ts

---
+++ src/app/alumnos/home-alumnos/home-alumnos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-alumnos',
+  templateUrl: './home-alumnos.component.html',
+  styleUrls: ['./home-alumnos.component.css']
+})
+export class HomeAlumnosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
