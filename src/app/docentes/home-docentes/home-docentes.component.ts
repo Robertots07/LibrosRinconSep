725788be115727923290f7dd5a1d9c64f2719198
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-08 20:58:07 -0500

    Create home-docentes.component.ts

---
+++ src/app/docentes/home-docentes/home-docentes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-docentes',
+  templateUrl: './home-docentes.component.html',
+  styleUrls: ['./home-docentes.component.css']
+})
+export class HomeDocentesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
