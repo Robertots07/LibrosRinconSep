d9256d81829b3b3e0dbd30bcda5d5f3727c8cce3
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-08 20:59:10 -0500

    Update proceso.routes.ts

--- src/app/proceso/proceso.routes.ts
+++ src/app/proceso/proceso.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
-import { HomeProcesoComponent } from './home-proceso/home-proceso.component';
-
+import { HomeprocesoComponent } from './homeproceso/homeproceso.component';
+import { SobreprocesoComponent } from './sobreproceso/sobreproceso.component';
+import { IngresomatComponent } from './ingresomat/ingresomat.component';
+import { PreseleccionComponent } from './preseleccion/preseleccion.component';
+import { SeleccionComponent } from './seleccion/seleccion.component';
 
 
 export const PROCESO_ROUTES: Routes = [
 
-    { path: 'HomeProceso', component: HomeProcesoComponent },
-    { path: '**', component: HomeProcesoComponent }
+    { path: 'Homeproceso', component: HomeprocesoComponent },
+    { path: 'Sobreproceso',component: SobreprocesoComponent },
+    { path: 'Ingresomat', component: IngresomatComponent },
+    { path: 'Preseleccion', component: PreseleccionComponent },
+    { path: 'Seleccion', component: SeleccionComponent },
+    { path: '**', component: HomeprocesoComponent }
   ];
