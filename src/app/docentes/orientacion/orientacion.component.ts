c57806c66118d1780a800ddecbc1adde9071afba
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-08 20:58:17 -0500

    Create orientacion.component.ts

---
+++ src/app/docentes/orientacion/orientacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orientacion',
+  templateUrl: './orientacion.component.html',
+  styleUrls: ['./orientacion.component.css']
+})
+export class OrientacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
