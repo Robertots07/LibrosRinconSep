4230c368589e546f2c92a9266ef00f7e3368f92f
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-09 17:59:48 -0500

    Create archivos.component.ts

---
+++ src/app/archivos/archivos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-archivos',
+  templateUrl: './archivos.component.html',
+  styleUrls: ['./archivos.component.css']
+})
+export class ArchivosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
