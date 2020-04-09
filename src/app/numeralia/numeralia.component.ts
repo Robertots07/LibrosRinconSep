838bcfc198cd3a43d5b7cb655e40ad7a05ec5cf8
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-09 18:00:33 -0500

    Create numeralia.component.ts

---
+++ src/app/numeralia/numeralia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-numeralia',
+  templateUrl: './numeralia.component.html',
+  styleUrls: ['./numeralia.component.css']
+})
+export class NumeraliaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
