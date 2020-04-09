f6c2af4b82819a0f06c12eaeff20a1c2aa57b5ab
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-08 20:59:07 -0500

    Create preseleccion.component.ts

---
+++ src/app/proceso/preseleccion/preseleccion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-preseleccion',
+  templateUrl: './preseleccion.component.html',
+  styleUrls: ['./preseleccion.component.css']
+})
+export class PreseleccionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
