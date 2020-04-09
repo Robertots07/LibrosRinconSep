ea169a6bb423f6de513f0763e88fb77dbaf46b85
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-09 18:00:12 -0500

    Create bibliotecas-distribuidas.component.ts

---
+++ src/app/numeralia/bibliotecas-distribuidas/bibliotecas-distribuidas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bibliotecas-distribuidas',
+  templateUrl: './bibliotecas-distribuidas.component.html',
+  styleUrls: ['./bibliotecas-distribuidas.component.css']
+})
+export class BibliotecasDistribuidasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
