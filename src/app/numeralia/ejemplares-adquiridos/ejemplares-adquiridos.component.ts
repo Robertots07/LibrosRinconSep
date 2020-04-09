8f99d8e9f79a7de77c534b4e48adee29c7d2dec7
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-09 17:59:49 -0500

    Create ejemplares-adquiridos.component.ts

---
+++ src/app/numeralia/ejemplares-adquiridos/ejemplares-adquiridos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ejemplares-adquiridos',
+  templateUrl: './ejemplares-adquiridos.component.html',
+  styleUrls: ['./ejemplares-adquiridos.component.css']
+})
+export class EjemplaresAdquiridosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
