0388dfd03e311b5ffd45cc7bb198cf155e49097d
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-08 20:59:29 -0500

    Create sobreproceso.component.ts

---
+++ src/app/proceso/sobreproceso/sobreproceso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sobreproceso',
+  templateUrl: './sobreproceso.component.html',
+  styleUrls: ['./sobreproceso.component.css']
+})
+export class SobreprocesoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
