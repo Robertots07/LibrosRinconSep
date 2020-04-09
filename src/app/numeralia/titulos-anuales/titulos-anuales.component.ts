86be4b4274e5f98d654d6ca584a5af57c47b69da
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-09 18:00:23 -0500

    Create titulos-anuales.component.ts

---
+++ src/app/numeralia/titulos-anuales/titulos-anuales.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-titulos-anuales',
+  templateUrl: './titulos-anuales.component.html',
+  styleUrls: ['./titulos-anuales.component.css']
+})
+export class TitulosAnualesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
