c5307a949e23fced634bab762ce5b17d38b964ce
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-08 20:58:58 -0500

    Create ingresomat.component.ts

---
+++ src/app/proceso/ingresomat/ingresomat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingresomat',
+  templateUrl: './ingresomat.component.html',
+  styleUrls: ['./ingresomat.component.css']
+})
+export class IngresomatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
