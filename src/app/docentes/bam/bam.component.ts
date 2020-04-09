6e5365099ba12f3a0caac78347b1d2608e0a5dfe
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-08 20:57:22 -0500

    Create bam.component.ts

---
+++ src/app/docentes/bam/bam.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bam',
+  templateUrl: './bam.component.html',
+  styleUrls: ['./bam.component.css']
+})
+export class BamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
