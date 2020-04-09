3c63a6bdf4484fddb550cbe9ecea4fbc64cd094c
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-08 20:58:46 -0500

    Create homeproceso.component.ts

---
+++ src/app/proceso/homeproceso/homeproceso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homeproceso',
+  templateUrl: './homeproceso.component.html',
+  styleUrls: ['./homeproceso.component.css']
+})
+export class HomeprocesoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
