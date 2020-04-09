3a88eae55a6b0d2b211e6cbc03fdacfd6f0e6493
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-09 17:59:57 -0500

    Create home-num.component.ts

---
+++ src/app/numeralia/home-num/home-num.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-num',
+  templateUrl: './home-num.component.html',
+  styleUrls: ['./home-num.component.css']
+})
+export class HomeNumComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
