7725a0b66b4c2877314af19691c932f5fe7e91bb
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-08 20:57:02 -0500

    Create libros-sugeridos.component.ts

---
+++ src/app/alumnos/libros-sugeridos/libros-sugeridos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-libros-sugeridos',
+  templateUrl: './libros-sugeridos.component.html',
+  styleUrls: ['./libros-sugeridos.component.css']
+})
+export class LibrosSugeridosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
