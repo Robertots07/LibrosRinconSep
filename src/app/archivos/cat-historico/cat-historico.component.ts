eb63a8947050268d741213e6914b5809bb97b499
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-10 17:00:49 -0500

    Create cat-historico.component.ts

---
+++ src/app/archivos/cat-historico/cat-historico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cat-historico',
+  templateUrl: './cat-historico.component.html',
+  styleUrls: ['./cat-historico.component.css']
+})
+export class CatHistoricoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
