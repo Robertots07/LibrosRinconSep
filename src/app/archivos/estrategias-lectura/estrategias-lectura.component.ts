c467cd4ccb51b2fec714a44742ec0004b5174a39
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-10 17:01:15 -0500

    Create estrategias-lectura.component.ts

---
+++ src/app/archivos/estrategias-lectura/estrategias-lectura.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estrategias-lectura',
+  templateUrl: './estrategias-lectura.component.html',
+  styleUrls: ['./estrategias-lectura.component.css']
+})
+export class EstrategiasLecturaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
