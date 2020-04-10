609f4f3b9d6533743220f02e3a999ea7c1c11b76
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-10 17:00:54 -0500

    Create home-archivos.component.ts

---
+++ src/app/archivos/home-archivos/home-archivos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-archivos',
+  templateUrl: './home-archivos.component.html',
+  styleUrls: ['./home-archivos.component.css']
+})
+export class HomeArchivosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
