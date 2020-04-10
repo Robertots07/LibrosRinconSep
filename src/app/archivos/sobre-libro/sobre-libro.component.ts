a11cd55630f9fcbfa9d9c0c6a5fac88db8db50ad
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-10 17:01:02 -0500

    Create sobre-libro.component.ts

---
+++ src/app/archivos/sobre-libro/sobre-libro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre-libro',
+  templateUrl: './sobre-libro.component.html',
+  styleUrls: ['./sobre-libro.component.css']
+})
+export class SobreLibroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
