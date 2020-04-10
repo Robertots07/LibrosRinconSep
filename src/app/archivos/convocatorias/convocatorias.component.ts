ec408b4e69d7d37d16d75c6088183372c19d06f7
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-10 17:01:23 -0500

    Create convocatorias.component.ts

---
+++ src/app/archivos/convocatorias/convocatorias.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-convocatorias',
+  templateUrl: './convocatorias.component.html',
+  styleUrls: ['./convocatorias.component.css']
+})
+export class ConvocatoriasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
