20bde334110a99ff06b9aa137b3f05e59cf9b14a
iuka <roberto.t37@gmail.com>
2020-04-16 13:12:38 -0500

    Update infolibro.service.ts

--- src/app/services/infolibro.service.ts
+++ src/app/services/infolibro.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class InfolibroService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  public infolibro(id: any){
+    return this.http.get('http://tsiete.com.mx/librosRincon/infolibro.php?id='+ id);
+  }
 }
+
+
+
