49e4031521451a2a805f0319d34ea0a51dd53e8a
iuka <roberto.t37@gmail.com>
2020-04-16 13:12:32 -0500

    Update modalficha.component.ts

--- src/app/componentes/modalficha/modalficha.component.ts
+++ src/app/componentes/modalficha/modalficha.component.ts
@@ -1,6 +1,7 @@
 
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { InfolibroService} from '../../services/infolibro.service'
 @Component({
   selector: 'app-modalficha',
   templateUrl: './modalficha.component.html',
@@ -8,14 +9,22 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class ModalfichaComponent implements OnInit {
 
-  @Input() fromParent;
+  @Input() fromParent: { id: any; };
+  por: any[];
+
+ 
  
   constructor(
-    public activeModal: NgbActiveModal
+    public activeModal: NgbActiveModal,
+    private infolib : InfolibroService
   ) { }
  
   ngOnInit() {
     console.log(this.fromParent);
+    this.infolib.infolibro(this.fromParent.id).subscribe((data: any[])=>{
+      console.log(data);
+      this.por = data;
+    }) 
   }
  
   closeModal(sendData) {
