f2e373d7d77bc9853164d35f03d2764c191cf32b
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-23 12:18:07 -0500

    Create sidebar-archivos.component.ts

---
+++ src/app/componentes/sidebar-archivos/sidebar-archivos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-archivos',
+  templateUrl: './sidebar-archivos.component.html',
+  styleUrls: ['./sidebar-archivos.component.css']
+})
+export class SidebarArchivosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
