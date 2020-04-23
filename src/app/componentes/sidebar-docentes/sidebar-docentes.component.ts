66864bf8c01f443d4bd8ce794f29c08645a4cdbf
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-23 12:18:15 -0500

    Create sidebar-docentes.component.ts

---
+++ src/app/componentes/sidebar-docentes/sidebar-docentes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-docentes',
+  templateUrl: './sidebar-docentes.component.html',
+  styleUrls: ['./sidebar-docentes.component.css']
+})
+export class SidebarDocentesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
