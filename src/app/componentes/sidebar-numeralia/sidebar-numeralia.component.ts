420a9bcdeb3d554b0b301253ed763b5e1da0efd9
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-24 17:46:37 -0500

    Create sidebar-numeralia.component.ts

---
+++ src/app/componentes/sidebar-numeralia/sidebar-numeralia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-numeralia',
+  templateUrl: './sidebar-numeralia.component.html',
+  styleUrls: ['./sidebar-numeralia.component.css']
+})
+export class SidebarNumeraliaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
