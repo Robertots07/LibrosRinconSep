7696185a4bdc8eb2a6d1720303c689a1f5dc2c06
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-04-24 17:47:00 -0500

    Create sidebar-proceso.component.ts

---
+++ src/app/componentes/sidebar-proceso/sidebar-proceso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-proceso',
+  templateUrl: './sidebar-proceso.component.html',
+  styleUrls: ['./sidebar-proceso.component.css']
+})
+export class SidebarProcesoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
