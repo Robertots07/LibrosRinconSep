30e9a894348c78438bf0085d1f61a7c5085caea5
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-05-06 16:52:28 -0500

    Update docentes.routes.ts

--- src/app/docentes/docentes.routes.ts
+++ src/app/docentes/docentes.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeDocentesComponent } from './home-docentes/home-docentes.component';
 import { BamComponent } from './bam/bam.component';
 import { OrientacionComponent } from './orientacion/orientacion.component';
+import { OrientacionesInstalacionComponent } from './orientaciones-instalacion/orientaciones-instalacion.component';
 
 export const DOCENTES_ROUTES: Routes = [
 
     { path: 'HomeDocentes', component: HomeDocentesComponent },
     { path: 'Bam', component: BamComponent },
     { path: 'Orientacion', component: OrientacionComponent },
+    { path: 'OrientacionesInstalacion', component: OrientacionComponent },
     { path: '**', component: HomeDocentesComponent }
   ];
