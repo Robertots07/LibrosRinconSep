14039866d777a71abf89feab81d6c91f3f051fa5
rebecobarrubias <36831403+rebecobarrubias@users.noreply.github.com>
2020-05-06 16:52:40 -0500

    Create orientaciones-instalacion.component.ts

---
+++ src/app/docentes/orientaciones-instalacion/orientaciones-instalacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orientaciones-instalacion',
+  templateUrl: './orientaciones-instalacion.component.html',
+  styleUrls: ['./orientaciones-instalacion.component.css']
+})
+export class OrientacionesInstalacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
